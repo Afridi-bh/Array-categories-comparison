561262672331b2a0ba1f9244a3837068d410aeb2
Afridi <141439658+Afridi-bh@users.noreply.github.com>
2025-05-03 13:19:36 +0600

    Delete Heap-dynamic .py

--- Heap-dynamic .py
+++
@@ -1,7 +0,0 @@
-def heap_dynamic_array():
-    arr = []  # dynamic and heap allocated
-    for i in range(10):
-        arr.append(i * 3)  # can grow at runtime
-    print("Heap Dynamic:", arr)
-
-heap_dynamic_array()
