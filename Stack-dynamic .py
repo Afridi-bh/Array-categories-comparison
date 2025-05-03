7038cf03688d5e5a32a8493918740029f255019f
Afridi <141439658+Afridi-bh@users.noreply.github.com>
2025-05-03 13:20:02 +0600

    Delete Stack-dynamic .py

--- Stack-dynamic .py
+++
@@ -1,9 +0,0 @@
-def stack_dynamic_array(size):
-    arr = [None] * size  # runtime size from function input
-    for i in range(size):
-        arr[i] = i + 1
-    print("Stack Dynamic:", arr)
-
-stack_dynamic_array(7)
-
-
