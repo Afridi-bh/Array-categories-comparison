e439c2a880ffd3bc93e269775978d2cfd2565294
Afridi <141439658+Afridi-bh@users.noreply.github.com>
2025-05-03 13:19:26 +0600

    Delete Fixed-size, Stack-dynamic.py

--- Fixed-size, Stack-dynamic.py
+++
@@ -1,9 +0,0 @@
-def fixed_stack_dynamic_array():
-    size = 5  # fixed size known at function runtime
-    arr = [0] * size  # stack-dynamic in concept
-    for i in range(size):
-        arr[i] = i * 10
-    print("Fixed Stack Dynamic:", arr)
-
-fixed_stack_dynamic_array()
-
